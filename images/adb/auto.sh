fa32e88fdf5bf15633222783cbae8a107f1fa0a0
zian <zian@zi.an>
2023-06-21 16:30:03 +0800

    update adb dockerfile

--- images/adb/auto.sh
+++ images/adb/auto.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-date > ~\start.txt
-adb connect 192.168.10.9
+date > ~/start.txt
+adb connect 192.168.10.12
 
 sleep 10s
 
 for((;;))
 do 
 sleep 2
-let x=1900+$[$RANDOM%10]
+let x=2000+$[$RANDOM%10]
 let y=1000+$[$RANDOM%10]
 adb shell input tap $x $y;
 #echo $x $y

95f4a365f9e49ed4f8a7c6ee3ce4bcccfd624c6e
zian <zian@zi.an>
2023-10-15 04:25:03 +0800

    auto update maomi2 shell

---
+++ images/nginx1.22/maomiav2/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+url=`tail -n1 /url.txt`
+url=`curl https://www.$url.com/ -o /dev/null -sw %{redirect_url}`
+url=`echo $url|awk -F '.' '{printf $2}'`
+if [ ${#url} -gt 6 ] ; then
+  sed "s|maomi.com|www.$url.com|g" /default > /etc/nginx/sites-enabled/default
+  date >> /url.txt
+  echo $url >> /url.txt
+  nginx -s reload
+  echo 'update is ok'
+fi

85c12789e13de3ba5f3afdbb1eba8746ed7f7552
zian <zian@zi.an>
2023-10-15 10:14:22 +0800

    update proxy store for maomi2

--- images/nginx1.22/maomiav2/update.sh
+++ images/nginx1.22/maomiav2/update.sh
@@ -4,7 +4,7 @@ url=`tail -n1 /url.txt`
 url=`curl https://www.$url.com/ -o /dev/null -sw %{redirect_url}`
 url=`echo $url|awk -F '.' '{printf $2}'`
 if [ ${#url} -gt 6 ] ; then
-  sed "s|maomi.com|www.$url.com|g" /default > /etc/nginx/sites-enabled/default
+  sed "s|www.maomi.com|www.$url.com|g" /default > /etc/nginx/sites-enabled/default
   date >> /url.txt
   echo $url >> /url.txt
   nginx -s reload

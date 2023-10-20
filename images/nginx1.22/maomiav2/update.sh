3443c8e0f7b35460bb0bf575875f5b55be3ea841
zian <zian@zi.an>
2023-10-20 12:27:23 +0800

    update lf

--- images/nginx1.22/maomiav2/update.sh
+++ images/nginx1.22/maomiav2/update.sh
@@ -3,10 +3,15 @@
 url=`tail -n1 /url.txt`
 url=`curl https://www.$url.com/ -o /dev/null -sw %{redirect_url}`
 url=`echo $url|awk -F '.' '{printf $2}'`
-if [ ${#url} -gt 6 ] ; then
+
+last=`tail -n1 /url.txt`
+
+if [ $url != $last ] ; then
   sed "s|www.maomi.com|www.$url.com|g" /default > /etc/nginx/sites-enabled/default
   date >> /url.txt
   echo $url >> /url.txt
   nginx -s reload
   echo 'update is ok'
+else
+  echo 'no change'
 fi

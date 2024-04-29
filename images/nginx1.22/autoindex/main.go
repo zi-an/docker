da5f1d46da4a746194afbe56a325985bc703c18d
zian <zian@zi.an>
2024-04-29 23:24:09 +0800

    update autoindex ffmpeg

--- images/nginx1.22/autoindex/main.go
+++ images/nginx1.22/autoindex/main.go
@@ -13,15 +13,26 @@ import (
 
 func main() {
 	server := &http.Server{
-		Addr: "127.0.0.1:8888",
+		Addr: "0.0.0.0:8888",
 	}
 	mux := http.NewServeMux()
+	mux.HandleFunc("/get", get)
 	mux.HandleFunc("/post", post)
 	mux.HandleFunc("/m3u8er", m3u8er)
 	server.Handler = mux
 	server.ListenAndServe()
 }
 
+//获取客户端信息
+func get(w http.ResponseWriter, r *http.Request) {
+	write := "ip:" + r.Header.Get("X-Real-IP") + "\n"
+	for k, v := range r.Header {
+		write = write + k + ":" + strings.Join(v, ";") + "\n"
+	}
+	w.Write([]byte(write))
+}
+
+//文件上传
 func post(w http.ResponseWriter, r *http.Request) {
 	from := r.Header.Get("Referer")
 	from = from[strings.Index(from, "=/")+1 : len(from)] //获取相对地址
@@ -54,11 +65,12 @@ func post(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//m3u8下载
 func m3u8er(w http.ResponseWriter, r *http.Request) {
 	uri := r.PostFormValue("uri")
-	uri,_=url.QueryUnescape(uri)
+	uri, _ = url.QueryUnescape(uri)
 	name := r.PostFormValue("name")
-        fmt.Println(uri,":",name)
+	fmt.Println(uri, ":", name)
 	exec.Command("m3u8downloader", uri, name).Run()
 	http.Redirect(w, r, "/?from=/m3u8/", http.StatusSeeOther)
 }
@@ -67,6 +79,7 @@ func m3u8er(w http.ResponseWriter, r *http.Request) {
 使用nginx代理
 location /post {proxy_pass http://127.0.0.1:8888;client_max_body_size 8192m;}
 location /m3u8 {proxy_pass http://127.0.0.1:8888;}
+location /get {proxy_pass http://127.0.0.1:8888;}
 
 测试数据
 curl http://127.0.0.1:8888/post -F file=@bank.jpg -H "Referer: http://127.0.0.1/?from=/upload/"

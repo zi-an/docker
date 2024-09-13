476a8d866b7784ba1582583b9bcd280d5564bd23
zian <zian@zi.an>
2024-09-13 08:12:26 +0800

    update configs

--- 3.images/nginx1.22/autoindex/main.go
+++ 3.images/nginx1.22/autoindex/main.go
@@ -19,29 +19,30 @@ func main() {
 	mux.HandleFunc("/get", get)
 	mux.HandleFunc("/post", post)
 	mux.HandleFunc("/m3u8er", m3u8er)
+	mux.HandleFunc("/noter", noter)
 	server.Handler = mux
-	server.ListenAndServe()
+	_ = server.ListenAndServe()
 }
 
-//获取客户端信息
+// 获取客户端信息
 func get(w http.ResponseWriter, r *http.Request) {
 	write := "ip:" + r.Header.Get("X-Real-IP") + "\n"
 	for k, v := range r.Header {
 		write = write + k + ":" + strings.Join(v, ";") + "\n"
 	}
-	w.Write([]byte(write))
+	_, _ = w.Write([]byte(write))
 }
 
-//文件上传
+// 文件上传
 func post(w http.ResponseWriter, r *http.Request) {
 	from := r.Header.Get("Referer")
 	from = from[strings.Index(from, "=/")+1 : len(from)] //获取相对地址
 
 	switch r.Method {
 	case "GET":
-		w.Write([]byte(string("method is GET")))
+		_, _ = w.Write([]byte(string("method is GET")))
 	case "POST":
-		r.ParseForm()
+		_ = r.ParseForm()
 		imgFile, _, _ := r.FormFile("file") //获取文件内容
 
 		defer imgFile.Close()
@@ -52,12 +53,12 @@ func post(w http.ResponseWriter, r *http.Request) {
 		_, err := os.Stat(imgName) //判断文件是否存在,在则隐藏并添加时间戳
 		if err == nil {
 			newName := "." + from + "." + files["file"][0].Filename + "." + time.Now().Format("20060102_150405")
-			os.Rename(imgName, newName)
+			_ = os.Rename(imgName, newName)
 		}
 
 		saveFile, _ := os.Create(imgName)
 		defer saveFile.Close()
-		io.Copy(saveFile, imgFile) //保存
+		_, _ = io.Copy(saveFile, imgFile) //保存
 
 		http.Redirect(w, r, "/?from="+from, http.StatusSeeOther)
 	default:
@@ -65,14 +66,21 @@ func post(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//m3u8下载
+// m3u8下载
 func m3u8er(w http.ResponseWriter, r *http.Request) {
 	uri := r.PostFormValue("uri")
 	uri, _ = url.QueryUnescape(uri)
 	name := r.PostFormValue("name")
 	fmt.Println(uri, ":", name)
-	exec.Command("m3u8downloader", uri, name).Run()
+	_ = exec.Command("m3u8downloader", uri, name).Run()
 	http.Redirect(w, r, "/?from=/m3u8/", http.StatusSeeOther)
+}
+
+func noter(w http.ResponseWriter, r *http.Request) {
+	note := r.PostFormValue("note")
+	file, _ := os.OpenFile("/home/nginx/note/note.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	_, _ = file.WriteString(time.Now().Format("01-02 15:04") + note + "\n")
+	http.Redirect(w, r, "/?from=/note/", http.StatusSeeOther)
 }
 
 /*
